cecd86b28315d6ea6a41344b8fd7b8d54bff614d
Koinunopochi <koinunopoti@gmail.com>
2024-06-30 01:38:50 +0900

    add：最低限のCRAD操作が可能なコンソールを作成した

--- backend/src/application/ExamService.ts
+++ backend/src/application/ExamService.ts
@@ -1,10 +1,38 @@
 import { Exam } from '../domain/entities/Exam';
+import yaml from 'js-yaml';
 import { ExamRepository } from '../domain/repositories/ExamRepository';
 
 export class ExamService {
   constructor(private examRepository: ExamRepository) {}
 
-  async getExam(filename:string): Promise<Exam> {
-    return this.examRepository.getExam(filename);
+  async getExam(filename: string, format: string): Promise<Exam | string> {
+    const exam = await this.examRepository.getExam(filename);
+
+    console.log(format);
+    // examを作る
+    if (format === 'yaml') {
+      return exam;
+    }
+    return yaml.load(exam) as Exam;
+  }
+
+  async getYamlMetaList(): Promise<object[]> {
+    return this.examRepository.getYamlMetaList();
+  }
+
+  async createExam(filename: string, content: string): Promise<void> {
+    return this.examRepository.createExam(filename, content);
+  }
+
+  async updateExam(
+    id: string,
+    filename: string,
+    content: string
+  ): Promise<void> {
+    return this.examRepository.updateExam(id, filename, content);
+  }
+
+  async deleteExam(id: string): Promise<void> {
+    return this.examRepository.deleteExam(id);
   }
 }
--- backend/src/presentation/ExamController.ts
+++ backend/src/presentation/ExamController.ts
@@ -6,15 +6,64 @@ export class ExamController {
 
   async getExam(req: Request, res: Response): Promise<void> {
     try {
-      const filename = req.query.filename as string;
-      console.log(filename);
-      const exam = await this.examService.getExam(filename);
+      console.log('Full request URL:', req.url);
+      const id = req.query.id as string;
+      const format = req.query.format as string;
+      console.log(format);
+      const exam = await this.examService.getExam(id, format);
       res.json(exam);
     } catch (error: any) {
       if (error.errno === -4058) {
         res.status(404).json({ error: 'File not found' });
         return;
       }
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  async getYamlMetaList(req: Request, res: Response): Promise<void> {
+    try {
+      const yamlIdList = await this.examService.getYamlMetaList();
+      console.log(yamlIdList);
+      res.json(yamlIdList);
+    } catch (error: any) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  async createExam(req: Request, res: Response): Promise<void> {
+    try {
+      const filename = req.body.name as string;
+      const content = req.body.content as string;
+      await this.examService.createExam(filename, content);
+      res.json({ message: 'Created' });
+    } catch (error: any) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  async updateExam(req: Request, res: Response): Promise<void> {
+    try {
+      const id = req.query.id as string;
+      const filename = req.body.name as string;
+      const content = req.body.content as string;
+      await this.examService.updateExam(id, filename, content);
+      res.json({ message: 'Updated' });
+    } catch (error: any) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  async deleteExam(req: Request, res: Response): Promise<void> {
+    try {
+      const id = req.query.id as string;
+      await this.examService.deleteExam(id);
+      res.json({ message: 'Deleted' });
+    } catch (error: any) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
