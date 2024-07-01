b72a5e86cb9a81d573f728b0fb605ddd07e0ebf2
Koinunopochi <koinunopoti@gmail.com>
2024-07-01 19:30:20 +0900

    add：mock的に簡単に追加した

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,127 +1,136 @@
 <!-- HomeView.vue -->
 <template>
   <main class="skill-check">
-    <h1>{{ meta.title }}</h1>
-    <Timer
-      :key="timerKey"
-      :duration="meta.limitTime"
-      @time-up="submitTest"
-      @half-time="handleHalfTime"
-      @one-minute-left="handleOneMinuteLeft"
-    />
+    <div v-if="!showResults">
+      <h1>{{ meta.title }}</h1>
+      <Timer
+        :key="timerKey"
+        :duration="meta.limitTime"
+        @time-up="submitTest"
+        @half-time="handleHalfTime"
+        @one-minute-left="handleOneMinuteLeft"
+      />
 
-    <div v-if="currentQuestion" class="question">
-      <h2>問題 {{ currentQuestionIndex + 1 }}:</h2>
-      <p>{{ currentQuestion.question }}</p>
+      <div v-if="currentQuestion" class="question">
+        <h2>問題 {{ currentQuestionIndex + 1 }}:</h2>
+        <p>{{ currentQuestion.question }}</p>
 
-      <!-- Single choice question -->
-      <div v-if="currentQuestion.type === 'single'" class="options">
-        <RadioButton
-          v-for="option in currentQuestion.options"
-          :key="option"
-          :label="option"
-          :value="option"
-          :name="'question-' + currentQuestion.id"
-          v-model="userAnswers.single"
-        />
-      </div>
+        <!-- Single choice question -->
+        <div v-if="currentQuestion.type === 'single'" class="options">
+          <RadioButton
+            v-for="option in currentQuestion.options"
+            :key="option"
+            :label="option"
+            :value="option"
+            :name="'question-' + currentQuestion.id"
+            v-model="userAnswers.single"
+          />
+        </div>
 
-      <!-- Multiple choice question -->
-      <div v-else-if="currentQuestion.type === 'multiple'" class="options">
-        <CheckboxButton
-          v-for="option in currentQuestion.options"
-          :key="option"
-          :label="option"
-          :value="option"
-          v-model="userAnswers.multiple[option]"
-        />
-      </div>
+        <!-- Multiple choice question -->
+        <div v-else-if="currentQuestion.type === 'multiple'" class="options">
+          <CheckboxButton
+            v-for="option in currentQuestion.options"
+            :key="option"
+            :label="option"
+            :value="option"
+            v-model="userAnswers.multiple[option]"
+          />
+        </div>
 
-      <!-- Code question -->
-      <div v-else-if="currentQuestion.type === 'code'" class="code-input">
-        <TextInput
-          :id="'code-input-' + currentQuestion.id"
-          v-model="userAnswers.code"
-          type="textarea"
-          rows="5"
-          placeholder="コードを入力してください"
-        />
-      </div>
+        <!-- Code question -->
+        <div v-else-if="currentQuestion.type === 'code'" class="code-input">
+          <TextInput
+            :id="'code-input-' + currentQuestion.id"
+            v-model="userAnswers.code"
+            type="textarea"
+            rows="5"
+            placeholder="コードを入力してください"
+          />
+        </div>
 
-      <div v-else-if="currentQuestion.type === 'sort'" class="sort-question">
-        <Sort :options="currentQuestion.options" v-model="userAnswers.sort" />
-      </div>
-      <!-- Free text question -->
-      <div
-        v-else-if="currentQuestion.type === 'free_text'"
-        class="free-text-input"
-      >
-        <TextInput
-          :id="'free-text-' + currentQuestion.id"
-          v-model="userAnswers.free_text"
-          type="textarea"
-          rows="5"
-          placeholder="自由に回答を記述してください"
-        />
-      </div>
-
-      <!-- Fill in the blank question -->
-      <div
-        v-else-if="currentQuestion.type === 'fill_in_blank'"
-        class="fill-in-blank"
-      >
-        <TextInput
-          v-for="(blank, index) in currentQuestion.blanks"
-          :key="index"
-          :id="'blank-' + currentQuestion.id + '-' + index"
-          v-model="userAnswers.fill_in_blank[index]"
-          :placeholder="`空欄${index + 1}`"
-        />
-      </div>
-
-      <!-- Matching question -->
-      <div v-else-if="currentQuestion.type === 'matching'" class="matching">
+        <div v-else-if="currentQuestion.type === 'sort'" class="sort-question">
+          <Sort :options="currentQuestion.options" v-model="userAnswers.sort" />
+        </div>
+        <!-- Free text question -->
         <div
-          v-for="(pair, index) in currentQuestion.matchingPairs"
-          :key="index"
-          class="matching-pair"
+          v-else-if="currentQuestion.type === 'free_text'"
+          class="free-text-input"
         >
-          <span>{{ pair.left }}</span>
-          <select v-model="userAnswers.matching[pair.left]">
-            <option
-              v-for="rightOption in shuffledRightOptions"
-              :key="rightOption"
-              :value="rightOption"
-            >
-              {{ rightOption }}
-            </option>
-          </select>
+          <TextInput
+            :id="'free-text-' + currentQuestion.id"
+            v-model="userAnswers.free_text"
+            type="textarea"
+            rows="5"
+            placeholder="自由に回答を記述してください"
+          />
+        </div>
+
+        <!-- Fill in the blank question -->
+        <div
+          v-else-if="currentQuestion.type === 'fill_in_blank'"
+          class="fill-in-blank"
+        >
+          <TextInput
+            v-for="(blank, index) in currentQuestion.blanks"
+            :key="index"
+            :id="'blank-' + currentQuestion.id + '-' + index"
+            v-model="userAnswers.fill_in_blank[index]"
+            :placeholder="`空欄${index + 1}`"
+          />
+        </div>
+
+        <!-- Matching question -->
+        <div v-else-if="currentQuestion.type === 'matching'" class="matching">
+          <div
+            v-for="(pair, index) in currentQuestion.matchingPairs"
+            :key="index"
+            class="matching-pair"
+          >
+            <span>{{ pair.left }}</span>
+            <select v-model="userAnswers.matching[pair.left]">
+              <option
+                v-for="rightOption in shuffledRightOptions"
+                :key="rightOption"
+                :value="rightOption"
+              >
+                {{ rightOption }}
+              </option>
+            </select>
+          </div>
         </div>
       </div>
+
+      <div class="navigation">
+        <Button
+          :primary="true"
+          @click="prevQuestion"
+          label="前の問題"
+          :square="true"
+          :disabled="currentQuestionIndex === 0"
+        />
+        <Button
+          :primary="true"
+          @click="nextQuestion"
+          label="次の問題"
+          :square="true"
+          :disabled="currentQuestionIndex === questions.length - 1"
+        />
+      </div>
+      <Button
+        :primary="true"
+        @click="submitTest"
+        label="終了"
+        :square="true"
+        background-color="#3a75ff"
+      />
     </div>
 
-    <div class="navigation">
-      <Button
-        :primary="true"
-        @click="prevQuestion"
-        label="前の問題"
-        :square="true"
-        :disabled="currentQuestionIndex === 0"
-      />
-      <Button
-        :primary="true"
-        @click="nextQuestion"
-        label="次の問題"
-        :square="true"
-        :disabled="currentQuestionIndex === questions.length - 1"
-      />
-    </div>
-    <Button
-      :primary="true"
-      @click="submitTest"
-      label="終了"
-      :square="true"
-      background-color="#3a75ff"
+    <TestResults
+      v-if="showResults"
+      :questions="questions"
+      :userAnswers="userAnswers"
+      :score="score"
     />
   </main>
 </template>
@@ -135,6 +144,7 @@ import CheckboxButton from '@/stories/Bases/CheckboxButtons/CheckboxButton.vue';
 import TextInput from '@/stories/Bases/TextInputs/TextInput.vue';
 import Timer from '@/stories/Bases/Timers/Timer.vue';
 import Sort from '@/stories/Bases/Sorts/Sort.vue';
+import TestResults from '@/stories/pages/TestResults.vue';
 
 const timerKey = ref(0);
 
@@ -207,7 +217,6 @@ function nextQuestion() {
   }
 }
 
-
 function calculateScore(): number {
   let totalScore = 0;
   questions.value.forEach((question) => {
@@ -255,17 +264,12 @@ function calculateScore(): number {
   return totalScore;
 }
 
+const showResults = ref(false);
+
 function submitTest() {
   const finalScore = calculateScore();
   score.value = finalScore;
-  const maxScore = questions.value.length;
-  alert(`テストが終了しました。\n最終スコア: ${finalScore}/${maxScore} 点`);
-
-  console.log('ユーザーの回答:', userAnswers.value);
-  console.log(
-    '正解:',
-    questions.value.map((q) => q.correctAnswer)
-  );
+  showResults.value = true;
 }
 
 watch(
